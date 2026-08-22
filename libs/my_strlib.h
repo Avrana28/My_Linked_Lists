7baa3d920d9d4fbd6a5566c9c89a8b75ff0b4398
Avrana28 <avrana.kern28@gmail.com>
2026-08-22 08:55:12 +0200

    main clean up: my_strlib.c my_strlib.h are not relative to the project

--- libs/my_strlib.h
+++
@@ -1,9 +0,0 @@
-#ifndef MYSTRLIB_H
-#define MYSTRLIB_H
-#include <stddef.h>
-
-size_t my_strlen(const char *s);
-char* my_strcpy(char *dest, const char *src);
-int my_strcmp(const char *s1, const char *s2);
-
-#endif // MYSTRLIB_H
